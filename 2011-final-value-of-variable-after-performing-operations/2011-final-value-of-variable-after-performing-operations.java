a381b9adb01a4d03938c3d1c8b9950f173d41d7b
OvO <syys1944@ewha.ac.kr>
2026-08-31 22:32:40 +0900

    Time: 1 ms (70.61%), Space: 44.4 MB (68.3%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int n=0;
+        for(int i = 0; i<operations.length; i++) {
+            if (operations[i].equals("X++") || operations[i].equals("++X")) {
+                n = n+1;
+            } else {
+                n = n-1;
+            }
+        }
+        return n;
+    }
+}
