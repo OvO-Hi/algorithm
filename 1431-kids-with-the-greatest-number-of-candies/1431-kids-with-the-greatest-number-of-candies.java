bd0b4ad9a85104a83c3e63ef5d9591cd7b9bc13d
OvO <syys1944@ewha.ac.kr>
2026-09-02 21:28:44 +0900

    Time: 1 ms (96.44%), Space: 43.3 MB (98.76%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+
+        for(int i =0; i<candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            } 
+        }
+
+        List<Boolean> check = new ArrayList<>();
+
+        for(int i=0; i<candies.length; i++) {
+            
+            if (candies[i] + extraCandies >= max ) {
+                check.add(true);
+            } else {
+                check.add(false);
+            }
+            
+        }
+        return check;
+
+    }
+}
